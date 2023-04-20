2972b77db91654c049ac72bc0b306b52fd9f1b72
Billy Mohajeri <billy.mohajeri@gmail.com>
2023-04-21 00:33:38 +0300

    Update shoppinglists controllers

--- app/controllers/shoppinglists_controller.rb
+++ app/controllers/shoppinglists_controller.rb
@@ -3,7 +3,7 @@ class ShoppinglistsController < ApplicationController
 
   def index
     @user = current_user
-    @food = @user.foods.left_outer_joins(:recipe_foods).where(recipe_foods: { id: nil })
+    @food = @user.foods.left_outer_joins(:recipe_foods).where(recipe_foods: { id: nil }).includes(:recipe_foods)
     @food_counts = @food.count
     @food_prices = @food.sum(:price)
   end
